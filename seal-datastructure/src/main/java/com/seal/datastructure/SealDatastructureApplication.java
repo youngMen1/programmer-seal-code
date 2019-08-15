c15c330a06649ef0a1282cff5724a8bc913748b6
youngMen1 <fengzhiqiangzxx199644>
2019-08-15 18:12:36 +0800

    desc:数据结构

---
+++ seal-datastructure/src/main/java/com/seal/datastructure/SealDatastructureApplication.java
@@ -0,0 +1,19 @@
+package com.seal.datastructure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhiqiang.feng
+ * @version 1.0
+ * @date-time 2019/02/13-11:01
+ * @description 数据结构
+ **/
+@SpringBootApplication
+public class SealDatastructureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SealDatastructureApplication.class, args);
+    }
+
+}
---
+++ seal-datastructure/src/main/java/com/seal/datastructure/controller/TestController.java
@@ -0,0 +1,19 @@
+package com.seal.datastructure.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author zhiqiang.feng
+ * @version 1.0
+ * @date-time 2019/8/15 18:06
+ * @description
+ **/
+@RestController
+public class TestController {
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
