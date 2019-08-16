bca2369b79dec38be67eea58f42ceee28edeca57
youngMen1 <fengzhiqiangzxx199644>
2019-08-16 10:39:45 +0800

    desc:数据结构

--- seal-datastructure/src/main/java/com/seal/datastructure/controller/DataStructureController.java
+++ seal-datastructure/src/main/java/com/seal/datastructure/controller/DataStructureController.java
@@ -6,10 +6,10 @@ import org.springframework.web.bind.annotation.RestController;
  * @author zhiqiang.feng
  * @version 1.0
  * @date-time 2019/8/15 18:06
- * @description
+ * @description 数据结构
  **/
 @RestController
-public class TestController {
+public class DataStructureController {
 
     public static void main(String[] args) {
 
