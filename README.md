618c93ff8966a532f903168b8f9c19304eed7857
youngMen1 <fengzhiqiangzxx199644>
2020-06-16 15:31:26 +0800

    desc:整理

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
-## 数据结构
-
-## 常用算法
-
-## 并发
+# 模块说明
+seal-algorithm：算法
+seal-code：平常积累的测试代码
+seal-common：封装公共类学习
+seal-concurrent：并发学习
+seal-datastructure：数据结构                            
--- seal-concurrent/src/main/java/com/seal/concurrent/SealConcurrentApplication.java
+++ seal-concurrent/src/main/java/com/seal/concurrent/SealConcurrentApplication.java
@@ -4,10 +4,9 @@ import com.seal.commonswagger.annotation.EnableSealSwagger2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 /**
+ * 并发学习
  * @author zhiqiang.feng
- * @version 1.0
  * @date-time 2019/02/13-11:01
- * @description 并发
  **/
 @EnableSealSwagger2
 @SpringBootApplication
